127fb2a9f202d52119d271551c48ed9d5ee92512
liyanyan19940914 <921258351@qq.com>
2016-07-26 17:29:41 +0800

    creat answerGenerator

--- spec/answerGenerator-spec.js
+++ spec/answerGenerator-spec.js
@@ -1,3 +1,13 @@
-/**
- * Created by liyan on 16-7-26.
- */
+'use strict'
+const AnswerGenerator=require('../main/models/answer-generate');
+describe('Answer Generator',()=>{
+    it('should generate answer',()=>{
+        const isUnique=(item,index,array)=>{
+            return array.lastIndexOf(item)===index;
+        };
+        const answer=AnswerGenerator.generate();
+        expect(answer.length).toEqual(4);
+        expect(answer.every(isUnique)).toBeTruthy();
+        expect(AnswerGenerator.generate()).not.toEqual(AnswerGenerator.generate());
+    })
+})
