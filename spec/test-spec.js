67e456ade37f57b61e285212b77975518d73a5ec
liyanyan19940914 <921258351@qq.com>
2016-07-26 13:58:05 +0800

    refactor test-spec

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -3,18 +3,20 @@ const main = require('../main/main.js');
 describe('guess-number', ()=> {
     const input = '1234';
     it('judgeNumber', ()=> {
-        const number = '1234';
-        let result = main.judgeNumber(input, number);
-        expect(result).toEqual('4A0B');
-    });
-    it('judgeNumber', ()=> {
-        const number = '4321';
-        let result = main.judgeNumber(input, number);
-        expect(result).toEqual('0A4B');
-    });
-    it('judgeNumber', ()=> {
-        const number = '5678';
-        let result = main.judgeNumber(input, number);
-        expect(result).toEqual('0A0B');
+        [
+            {
+                number:'1234',
+                expectResult:'4A0B'},
+            {
+                number:'4321',
+                expectResult:'0A4B'
+            },
+            {
+                number:'5678',
+                expectResult:'0A0B'
+            }
+        ].forEach(item=>{
+            expect( main.judgeNumber(input, item.number)).toEqual(item.expectResult);
+        })
     });
 })
